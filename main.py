67ab21a84f7bd7b0573e4d2bbf40dce180ca4573
gr1zer1 <timuravramenko99@gmail.com>
2026-05-01 21:38:00 +0200

    add task class

---
+++ main.py
@@ -0,0 +1,4 @@
+def main(*args,**kwargs):
+    print(kwargs,args)
+
+main(15,21,a=12,b=13)
---
+++ task.py
@@ -0,0 +1,23 @@
+import uuid
+from typing import Any
+from enum import StrEnum
+
+class TaskStatus(StrEnum):
+    PENDING = "pending"
+    RUNNING = "running"
+    DONE = "done"
+    FAILED = "failed"
+
+
+class Task:
+    def __init__(self, function_name:str, args, kwargs):
+        """
+        status = pending(by default), running, done, failed
+        """
+        self.id: uuid.UUID = uuid.uuid4()
+        self.function_name = function_name
+        self.args = args
+        self.kwargs = kwargs
+        self.result: None | Any = None 
+        self.status: TaskStatus = TaskStatus.PENDING
+
